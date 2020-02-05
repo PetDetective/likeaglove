c184cbbbf49e7d1f4756aeaf9db3dff5ad9d3476
AceVentura <PetDetective@likeaglove.com>
2020-02-05 22:08:49 +0100

    Adding some javadoc and configuration comments in core-quarkus

--- core-quarkus/src/main/java/com/likeaglove/core/quarkus/resource/model/ParkingBillRestAPI.java
+++ core-quarkus/src/main/java/com/likeaglove/core/quarkus/resource/model/ParkingBillRestAPI.java
@@ -14,14 +14,27 @@ public class ParkingBillRestAPI {
 
 	private ParkingBill parkingBill;
 
+	/**
+	 * Default constructor.
+	 */
 	public ParkingBillRestAPI() {
 		parkingBill = new ParkingBill();
 	}
 
+	/**
+	 * Get the amount to pay
+	 * 
+	 * @return the amount to pay
+	 */
 	public long getAmount() {
 		return parkingBill.getAmount();
 	}
 
+	/**
+	 * Define the amount to pay
+	 * 
+	 * @param amount to pay
+	 */
 	public void setAmount(long amount) {
 		parkingBill.setAmount(amount);
 	}
--- core-quarkus/src/test/java/com/likeaglove/core/quarkus/resource/NativeParkingResourceIT.java
+++ core-quarkus/src/test/java/com/likeaglove/core/quarkus/resource/NativeParkingResourceIT.java
@@ -2,6 +2,11 @@ package com.likeaglove.core.quarkus.resource;
 
 import io.quarkus.test.junit.NativeImageTest;
 
+/**
+ * To run the IT test in native mode
+ * @author AceVentura
+ *
+ */
 @NativeImageTest
 public class NativeParkingResourceIT extends ParkingResourceTest {
 
